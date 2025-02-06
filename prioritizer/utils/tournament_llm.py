e225ae38492b49207a70d11b3f759b3192b5c42d
piotrnowakowski <piotr98nowakowski@gmail.com>
2025-02-06 21:45:04 +0900

    switched into rounds

--- prioritizer/utils/tournament_llm.py
+++ prioritizer/utils/tournament_llm.py
@@ -24,6 +24,7 @@ class PrioritizedActions(BaseModel):
 
 # Function to send prompt to LLM
 def send_to_llm(prompt):
+    #TODO implement it as a conversation where previous part of the round is in memory 
     response = client.beta.chat.completions.parse(
         model="gpt-4o-2024-08-06",
         messages=[
@@ -33,6 +34,20 @@ def send_to_llm(prompt):
         temperature=0.0,
     )
     return response.choices[0].message.parsed
+
+def run_round(actions, city):
+    next_round = []
+    random.shuffle(actions)
+    for i in range(0, len(actions), 2):
+        if i + 1 < len(actions):
+            action1, action2 = actions[i], actions[i + 1]
+            prompt = return_prompt([action1, action2], city)
+            result = send_to_llm(prompt)
+            winner = result
+            next_round.append(winner)
+        else:
+            next_round.append(actions[i])
+    return next_round
 
 # Function to pair actions and rank them
 def pair_and_rank(actions, city):
@@ -46,32 +61,12 @@ def pair_and_rank(actions, city):
     Returns:
         List[dict]: Fully ranked top 5 actions.
     """
+
     # First round: Top 20 to Top 10
-    next_round = []
-    random.shuffle(actions)
-    for i in range(0, len(actions), 2):
-        if i + 1 < len(actions):
-            action1, action2 = actions[i], actions[i + 1]
-            prompt = return_prompt([action1, action2], city)
-            result = send_to_llm(prompt)
-            # In case there has to be anything else added here to make the code work
-            winner = result
-            next_round.append(winner)
-    actions = next_round
+    actions = run_round(actions, city)
 
     # Second round: Top 10 to Top 5
-    next_round = []
-    random.shuffle(actions)
-    for i in range(0, len(actions), 2):
-        if i + 1 < len(actions):
-            action1, action2 = actions[i], actions[i + 1]
-            prompt = return_prompt([action1, action2], city)
-            result = send_to_llm(prompt)
-            winner = result
-            next_round.append(winner)
-        else:
-            next_round.append(actions[i])
-    actions = next_round
+    actions = run_round(actions, city)
 
     # Final round: Rank Top 5
     rankings = []
